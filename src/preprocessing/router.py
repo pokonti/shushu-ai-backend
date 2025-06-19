286e409c9b63bdf8ee6ec193aab6c00befa39709
pokonti <pokonti@bk.ru>
2025-06-19 15:33:58 +0500

    corrected endpoints(but figniya)

--- src/preprocessing/router.py
+++ src/preprocessing/router.py
@@ -3,11 +3,10 @@ from fastapi.responses import JSONResponse
 from pathlib import Path
 
 from src.preprocessing.denoiser import denoise_audio
-from src.preprocessing.service import transcribe_audio, find_video_file
+from src.preprocessing.service import find_video_file
 from src.preprocessing.filler import (
-    get_filler_word_timestamps,
     remove_filler_words_from_audio,
-    remove_filler_words_from_video, align_transcripts, get_filler_timestamps_from_audio,
+    remove_filler_words_from_video, get_filler_timestamps_from_audio,
 )
 from src.video.service import replace_audio_in_video
 
@@ -17,33 +16,6 @@ UPLOAD_DIR = Path("video")
 UPLOAD_DIR.mkdir(exist_ok=True)
 AUDIO_DIR = Path("audio")
 AUDIO_DIR.mkdir(exist_ok=True)
-
-# @router.get("/denoise")
-# def run_denoising(media_id: str):
-#     audio_path = AUDIO_DIR / f"{media_id}.wav"
-#     video_path = find_video_file(media_id, UPLOAD_DIR)
-#
-#     try:
-#         if audio_path.exists():
-#             output_path = denoise_audio(str(audio_path))
-#             return JSONResponse(content={
-#                 "message": "Audio denoised successfully",
-#                 "output_path": output_path
-#             })
-#
-#         if video_path.exists():
-#             enhanced_video = replace_audio_in_video(str(video_path), f"{AUDIO_DIR}/{media_id}_cleaned.wav")
-#
-#             return JSONResponse(content={
-#                 "message": "Video denoised successfully",
-#                 "output_path": enhanced_video
-#             })
-#
-#         else:
-#             raise HTTPException(status_code=400, detail="You must provide either audio_path or video_path.")
-#
-#     except Exception as e:
-#         return JSONResponse(status_code=500, content={"error": str(e)})
 
 @router.get("/denoise")
 def run_denoising(media_id: str):
